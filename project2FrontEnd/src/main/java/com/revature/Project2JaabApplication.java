70dda371e756e1faded9156bf6c4a1d25698a318
keoffor <kenobago@gmail.com>
2022-03-07 15:18:56 -0600

    changes made for EmailApi

--- project2FrontEnd/src/main/java/com/revature/Project2JaabApplication.java
+++ project2FrontEnd/src/main/java/com/revature/Project2JaabApplication.java
@@ -4,12 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-<<<<<<< HEAD
 import org.springframework.web.client.RestTemplate;
-=======
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
->>>>>>> 43132a78d86b3164ef20ebc806441a699ff3069e
 
 @SpringBootApplication
 //@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
