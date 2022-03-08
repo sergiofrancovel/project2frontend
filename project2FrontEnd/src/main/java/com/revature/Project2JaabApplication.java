0160c4b40f3cdd67a77227898f5116c5d2690372
keoffor <kenobago@gmail.com>
2022-03-07 22:17:48 -0600

    added actuator

--- project2FrontEnd/src/main/java/com/revature/Project2JaabApplication.java
+++ project2FrontEnd/src/main/java/com/revature/Project2JaabApplication.java
@@ -2,13 +2,14 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 //@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class Project2JaabApplication {
 
